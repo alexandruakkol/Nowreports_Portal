e813e655b29bbecff28de564f29a27c113c7fd14
alexandruakkol <alexandru.akkol@gmail.com>
2024-02-24 14:56:29 -0800

    www. on prod addr for cookie pass

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {App} from './App';
 
 ////// ======= globals setup ======= \\\\\\
 window.appdata = {};
-window.appdata.API_ADDR = 'http://nowreports.com/api';
+window.appdata.API_ADDR = 'http://www.nowreports.com/api';
 window.appdata.AI_API_ADDR = 'http://127.0.0.1:5001'; //TODO
 window.appdata.SEC_BASE_ADDR = 'https://www.sec.gov/Archives/edgar/data/';
 
