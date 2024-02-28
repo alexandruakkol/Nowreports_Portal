703b1813cf7ade14c9cabb5eb2c317644b893e9f
alexandruakkol <alexandru.akkol@gmail.com>
2024-02-27 22:45:03 -0800

    dev env changes

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {App} from './App';
 window.appdata = {};
 window.appdata.API_ADDR = process.env.REACT_APP_ENV == 'production' 
     ? 'https://nowreports.com/api' 
-    : 'http://localhost:8005';
+    : '';
 window.appdata.AI_API_ADDR = 'http://127.0.0.1:5001'; //TODO
 window.appdata.SEC_BASE_ADDR = 'https://www.sec.gov/Archives/edgar/data/';
 
